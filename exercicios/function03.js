046dd0569c08045cc838fed7c850804b810e3a31
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2023-10-31 18:19:00 -0300

    function 03

---
+++ exercicios/function03.js
@@ -0,0 +1,10 @@
+/* escreva uma função em Javascript que realiza a conversão de uma temperatura fornecida em Graus Fahrenheit(F)
+para Celsius(C)
+
+Dica: para conveter uma temperatura em F para C, subtraia 32 da temperatura e multiplique o resultad por 0,5556(5/9)
+*/
+
+function  converter(){
+    console.log("Conversão de Celcius(C) para Fahrenheit(F)")
+
+}
