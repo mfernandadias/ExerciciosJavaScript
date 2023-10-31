49bd5bda0ef89dffc39da8cfe5294edf6ab2d0d9
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2023-10-30 22:38:50 -0300

    exercícios

---
+++ exercicios/funcion01.js
@@ -0,0 +1,7 @@
+/*
+escreva uma função em Javascript que mostre na tela a mensagem "Bom dia, Mundo!!!!"
+*/
+
+function text() {
+    document.write("Bom dia, Mundo!!!")
+}
---
+++ exercicios/function02.js
@@ -0,0 +1,14 @@
+/*
+escreva uma função que retorne o cubo de um número forneido pelo usuário 
+*/
+
+//const num = num; 
+/*num = 5;
+function soma(){
+    console.log(num * num * num);  
+}*/
+
+const num = 5; 
+function soma() {
+    console.log(num * num * num )
+}
