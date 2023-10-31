acbcd39e2ed5bcb9d3708428b48b89dc04da3802
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2023-10-31 20:45:23 -0300

    function 04

---
+++ exercicios/function05.js
@@ -0,0 +1,9 @@
+/* 
+/* escreva uma função que encontre  a área e o perimetro de um circuto, de acordo com o raio fornecido 
+*/
+
+function calculaCirculo(raio) {
+    return `Perímetro: ${2*Math.PI*raio} <br> Área: ${Math.PI*raio*raio}`;
+}
+
+console.log(calc)
